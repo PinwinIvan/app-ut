faea22b2126232c6bca57bad9847e7ab27593dc2
IvanCG <147391715+PinwinIvan@users.noreply.github.com>
2024-01-25 08:02:33 -0500

    inicial base project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatProgressBarModule, MatButtonModule, MatDividerModule, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'app-ut';
+  title = 'AMBATUKUUUUUUMMMMMMMMM';
+  longText = 'Pinwinazo'
 }
