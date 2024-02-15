df8cc277a72a174a285aae781d562e5c2ff9deef
IvanCG <147391715+PinwinIvan@users.noreply.github.com>
2024-02-14 19:20:51 -0500

    added form

--- src/app/foods/form-food/form-food.component.ts
+++ src/app/foods/form-food/form-food.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 
 @Component({
   selector: 'app-form-food',
   standalone: true,
-  imports: [FormControl, FormGroup, ReactiveFormsModule, Validators],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, FormControl, FormGroup],
   templateUrl: './form-food.component.html',
   styleUrl: './form-food.component.scss'
 })
-export class FormFoodComponent {
-
+export class FormFoodComponent{
+ 
+    addMenuItem= new FormGroup({
+      foodName: new FormControl(''),
+      foodCategory: new FormControl(''),
+      foodPrice: new FormControl(''),
+      foodDesc: new FormControl(''),
+      foodImage: new FormControl('')
+    })
 }
